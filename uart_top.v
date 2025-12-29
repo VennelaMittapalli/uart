399e6b7d7e01299e99f4f67af44603f93bbcd783
VennelaMittapalli <59090497+VennelaMittapalli@users.noreply.github.com>
2025-12-29 18:00:21 +0530

    Add UART top module with receiver and transmitter

--- uart_top.v
+++ uart_top.v
@@ -1,1 +1,9 @@
+module uart_top(input clk,reset,Rx,btn1,[7:0]sw, output Tx,[7:0]RxData);
 
+wire TxD_debug,transmit_debug,button_debug,clk_debug;
+
+uart_receiver rxr(clk,reset,Rx,RxData);
+
+uart transmiter(sw,reset,btn1,clk,Tx,TxD_debug,transmit_debug,button_debug,clk_debug); 
+
+endmodule
